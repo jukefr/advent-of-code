0fb0ad71d8e8b5ac79fc90cd5fba32d32757f1e9
kate <kate@juke.fr>
2024-12-05 09:42:49 +0100

    day5part2

--- 2024/day5/src/main.rs
+++ 2024/day5/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn part_1(rules: Vec<&str>, updates: Vec<&str>) -> u32 {
+fn part_1(rules: &Vec<&str>, updates: &Vec<&str>) -> u32 {
     let rules_numbers: Vec<Vec<u32>> = rules
         .iter()
         .map(|rule| {
@@ -45,7 +45,7 @@ fn part_1(rules: Vec<&str>, updates: Vec<&str>) -> u32 {
         .sum()
 }
 
-fn part_2(rules: Vec<&str>, updates: Vec<&str>) -> u32 {
+fn part_2(rules: &Vec<&str>, updates: &Vec<&str>) -> u32 {
     let rules_numbers: Vec<Vec<u32>> = rules
         .iter()
         .map(|rule| {
@@ -89,25 +89,29 @@ fn part_2(rules: Vec<&str>, updates: Vec<&str>) -> u32 {
         })
         .collect();
     let mut fixed_updates = invalid_updates.clone();
-    let mut updates_fixed = false;
-    while !updates_fixed {
-        updates_fixed = true;
+    let mut is_updates_fixed = false;
+    while !is_updates_fixed {
+        is_updates_fixed = true;
         let updated_invalid_updates: Vec<Vec<u32>> = fixed_updates
             .iter()
             .map(|invalid_update| {
                 rules_numbers
                     .iter()
-                    .fold(invalid_update.clone(), |mut updated, rule| {
-                        let (first, second) = (rule[0], rule[1]);
-                        let first_pos = updated.iter().position(|&page| page == first);
-                        let second_pos = updated.iter().position(|&page| page == second);
-                        if let (Some(f_pos), Some(s_pos)) = (first_pos, second_pos) {
-                            if f_pos > s_pos {
-                                updated.swap(f_pos, s_pos);
-                                updates_fixed = false;
+                    .fold(invalid_update.clone(), |mut update, rule| {
+                        let (first_rule, second_rule) = (rule[0], rule[1]);
+                        let first_page_position =
+                            update.iter().position(|&page| page == first_rule);
+                        let second_page_position =
+                            update.iter().position(|&page| page == second_rule);
+                        if let (Some(found_first_position), Some(found_second_position)) =
+                            (first_page_position, second_page_position)
+                        {
+                            if found_first_position > found_second_position {
+                                update.swap(found_first_position, found_second_position);
+                                is_updates_fixed = false;
                             }
                         }
-                        updated
+                        update
                     })
             })
             .collect();
@@ -124,85 +128,55 @@ fn main() {
     let (rules_string, updates_string) = contents.split_once("\n\n").unwrap();
     let rules: Vec<&str> = rules_string.lines().collect();
     let updates: Vec<&str> = updates_string.lines().collect();
-    println!("[Part 1] {}", part_1(rules.clone(), updates.clone()));
-    println!("[Part 2] {}", part_2(rules.clone(), updates.clone()));
+    println!("[Part 1] {}", part_1(&rules, &updates));
+    println!("[Part 2] {}", part_2(&rules, &updates));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    const TEST_INPUT: &str = "\
+        47|53
+        97|13
+        97|61
+        97|47
+        75|29
+        61|13
+        75|53
+        29|13
+        97|29
+        53|29
+        61|53
+        97|53
+        61|29
+        47|13
+        75|47
+        97|75
+        47|61
+        75|61
+        47|29
+        75|13
+        53|13
+
+        75,47,61,53,29
+        97,61,53,29,13
+        75,29,13
+        75,97,47,61,53
+        61,13,29
+        97,13,75,29,47
+    ";
     #[test]
     fn test_part_1() {
-        let test_input = "\
-            47|53
-            97|13
-            97|61
-            97|47
-            75|29
-            61|13
-            75|53
-            29|13
-            97|29
-            53|29
-            61|53
-            97|53
-            61|29
-            47|13
-            75|47
-            97|75
-            47|61
-            75|61
-            47|29
-            75|13
-            53|13
-
-            75,47,61,53,29
-            97,61,53,29,13
-            75,29,13
-            75,97,47,61,53
-            61,13,29
-            97,13,75,29,47
-        ";
-        let (rules_string, updates_string) = test_input.trim().split_once("\n\n").unwrap();
+        let (rules_string, updates_string) = TEST_INPUT.trim().split_once("\n\n").unwrap();
         let rules: Vec<&str> = rules_string.lines().collect();
         let updates: Vec<&str> = updates_string.lines().collect();
-        assert_eq!(part_1(rules, updates), 143);
+        assert_eq!(part_1(&rules, &updates), 143);
     }
     #[test]
     fn test_part_2() {
-        let test_input = "\
-            47|53
-            97|13
-            97|61
-            97|47
-            75|29
-            61|13
-            75|53
-            29|13
-            97|29
-            53|29
-            61|53
-            97|53
-            61|29
-            47|13
-            75|47
-            97|75
-            47|61
-            75|61
-            47|29
-            75|13
-            53|13
-
-            75,47,61,53,29
-            97,61,53,29,13
-            75,29,13
-            75,97,47,61,53
-            61,13,29
-            97,13,75,29,47
-        ";
-        let (rules_string, updates_string) = test_input.trim().split_once("\n\n").unwrap();
+        let (rules_string, updates_string) = TEST_INPUT.trim().split_once("\n\n").unwrap();
         let rules: Vec<&str> = rules_string.lines().collect();
         let updates: Vec<&str> = updates_string.lines().collect();
-        assert_eq!(part_2(rules, updates), 123);
+        assert_eq!(part_2(&rules, &updates), 123);
     }
 }
