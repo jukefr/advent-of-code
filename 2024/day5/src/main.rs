07130753699f86431eb2591b218c95205b3888ae
kate <kate@juke.fr>
2024-12-05 09:05:48 +0100

    day5part2wip

--- 2024/day5/src/main.rs
+++ 2024/day5/src/main.rs
@@ -45,12 +45,59 @@ fn part_1(rules: Vec<&str>, updates: Vec<&str>) -> u32 {
         .sum()
 }
 
+fn part_2(rules: Vec<&str>, updates: Vec<&str>) -> u32 {
+    let rules_numbers: Vec<Vec<u32>> = rules
+        .iter()
+        .map(|rule| {
+            rule.split('|')
+                .map(|page| page.trim())
+                .filter_map(|page| page.parse::<u32>().ok())
+                .collect()
+        })
+        .collect();
+    let updates_numbers: Vec<Vec<u32>> = updates
+        .iter()
+        .map(|update| {
+            update
+                .split(',')
+                .map(|page| page.trim())
+                .filter_map(|page| page.parse::<u32>().ok())
+                .collect()
+        })
+        .collect();
+    let invalid_updates: Vec<Vec<u32>> = updates_numbers
+        .iter()
+        .filter_map(|update_pages| {
+            let is_valid_update = rules_numbers.iter().all(|rule| {
+                let (first_rule, second_rule) = (rule[0], rule[1]);
+                let first_page_position = update_pages.iter().position(|&page| page == first_rule);
+                let second_page_position =
+                    update_pages.iter().position(|&page| page == second_rule);
+                if let (Some(found_first_position), Some(found_second_position)) =
+                    (first_page_position, second_page_position)
+                {
+                    found_first_position < found_second_position
+                } else {
+                    true
+                }
+            });
+            if !is_valid_update {
+                Some(update_pages.clone())
+            } else {
+                None
+            }
+        })
+        .collect();
+    0
+}
+
 fn main() {
     let contents = fs::read_to_string("input").unwrap();
     let (rules_string, updates_string) = contents.split_once("\n\n").unwrap();
     let rules: Vec<&str> = rules_string.lines().collect();
     let updates: Vec<&str> = updates_string.lines().collect();
-    println!("[Part 1] {}", part_1(rules, updates));
+    println!("[Part 1] {}", part_1(rules.clone(), updates.clone()));
+    println!("[Part 2] {}", part_2(rules.clone(), updates.clone()));
 }
 
 #[cfg(test)]
@@ -92,5 +139,42 @@ mod tests {
         let rules: Vec<&str> = rules_string.lines().collect();
         let updates: Vec<&str> = updates_string.lines().collect();
         assert_eq!(part_1(rules, updates), 143);
+    }
+    #[test]
+    fn test_part_2() {
+        let test_input = "\
+            47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+
+            75,47,61,53,29
+            97,61,53,29,13
+            75,29,13
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47
+        ";
+        let (rules_string, updates_string) = test_input.trim().split_once("\n\n").unwrap();
+        let rules: Vec<&str> = rules_string.lines().collect();
+        let updates: Vec<&str> = updates_string.lines().collect();
+        assert_eq!(part_2(rules, updates), 123);
     }
 }
