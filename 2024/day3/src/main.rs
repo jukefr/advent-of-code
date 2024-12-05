905faedda98be959c211f5a9ab82a09182f23814
kate <kate@juke.fr>
2024-12-05 10:02:57 +0100

    make day3 more rusty

--- 2024/day3/src/main.rs
+++ 2024/day3/src/main.rs
@@ -1,21 +1,21 @@
 use regex::Regex;
 use std::fs;
 
-fn part_1(input: &str) -> i32 {
-    let regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    input
+fn part_1(input_string: &str) -> i32 {
+    let multiplication_regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    input_string
         .lines()
-        .flat_map(|line| regex.captures_iter(line))
-        .filter_map(|capture| {
-            let x: i32 = capture[1].parse().ok()?;
-            let y: i32 = capture[2].parse().ok()?;
-            Some(x * y)
+        .flat_map(|line| multiplication_regex.captures_iter(line))
+        .filter_map(|capture_groups| {
+            let first_number: i32 = capture_groups[1].parse().ok()?;
+            let second_number: i32 = capture_groups[2].parse().ok()?;
+            Some(first_number * second_number)
         })
         .sum()
 }
 
-fn part_2(input: &str) -> i32 {
-    let regex = Regex::new(
+fn part_2(input_string: &str) -> i32 {
+    let action_regex = Regex::new(
         r"(?x)
         mul\((\d+),(\d+)\) |
         do\(\) |
@@ -23,23 +23,23 @@ fn part_2(input: &str) -> i32 {
     ",
     )
     .unwrap();
-    let mut is_enabled = true;
-    input
+    let mut is_action_enabled = true;
+    input_string
         .lines()
-        .flat_map(|line| regex.captures_iter(line))
-        .filter_map(|capture| match &capture[0] {
+        .flat_map(|line| action_regex.captures_iter(line))
+        .filter_map(|capture_groups| match &capture_groups[0] {
             "do()" => {
-                is_enabled = true;
+                is_action_enabled = true;
                 None
             }
             "don't()" => {
-                is_enabled = false;
+                is_action_enabled = false;
                 None
             }
-            _mul if is_enabled => {
-                let x = capture[1].parse::<i32>().ok()?;
-                let y = capture[2].parse::<i32>().ok()?;
-                Some(x * y)
+            _multiplication_action if is_action_enabled => {
+                let first_number = capture_groups[1].parse::<i32>().ok()?;
+                let second_number = capture_groups[2].parse::<i32>().ok()?;
+                Some(first_number * second_number)
             }
             _ => None,
         })
@@ -47,9 +47,9 @@ fn part_2(input: &str) -> i32 {
 }
 
 fn main() {
-    let input = fs::read_to_string("input").expect("Failed to read input file");
-    println!("[Part 1] {}", part_1(&input));
-    println!("[Part 2] {}", part_2(&input));
+    let input_file_contents = fs::read_to_string("input").expect("Failed to read input file");
+    println!("[Part 1] {}", part_1(&input_file_contents));
+    println!("[Part 2] {}", part_2(&input_file_contents));
 }
 
 #[cfg(test)]
@@ -57,13 +57,14 @@ mod tests {
     use super::*;
     #[test]
     fn test_part_1() {
-        let test_input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-        assert_eq!(part_1(test_input), 161);
+        let test_input_string =
+            "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        assert_eq!(part_1(test_input_string), 161);
     }
     #[test]
     fn test_part_2() {
-        let test_input =
+        let test_input_string =
             "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
-        assert_eq!(part_2(test_input), 48);
+        assert_eq!(part_2(test_input_string), 48);
     }
 }
